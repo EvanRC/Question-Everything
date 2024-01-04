33170226c220f4b2ccc42ea2cff45ceec2493b02
Clayton Hensley <chensley56@gmail.com>
2024-01-03 18:39:22 -0800

    added question template and difficulty setting

---
+++ app.js
@@ -0,0 +1,76 @@
+// Core dependencies
+const express = require('express');
+const http =  require('http');
+const path = require('path');
+
+// Session and authentication 
+const session = require('express-session');
+
+// Database and ORM
+const Sequelize = require('sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Template Engine
+const { engine } = require('express-handlebars');
+
+// Real-time communication
+const socketIO = require('socket.io');
+
+// Enviroment variables 
+require('dotenv').config();
+
+// Initialize Express app and HTTP server 
+const app = express();
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+const io = socketIO(server);
+
+// Setting up Sequalize with MySQL
+const sequelizeInstance = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false
+});
+
+// Session configuration
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: new SequelizeStore({
+        db: sequelizeInstance
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {}
+}));
+
+// Body Parser Middleware to handle JSON and URL encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Static files middleware (for CSS, JS, Images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handlebars Middleware 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+// Define routes 
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Get ready to Question Everything!'});
+});
+
+// Socket.IO connection
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('disconnect', () => {
+        console.log(' Client disconnected');
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
