8a945bb39d7ec47ad3e2193f04662deaf793bcee
Clayton Hensley <chensley56@gmail.com>
2024-01-04 18:41:47 -0800

    added 10 second time and alert at end of game

--- script.js
+++ script.js
@@ -1,126 +1,194 @@
-const categoryMapping = {
-    api1: 9,  // General Knowledge
-    api2: 21, // Sports
-    api3: 23, // History
-    api4: 27, // Animals
-    api5: 15, // Video Games
-    api6: 26, // Celebrities
-};
+    const categoryMapping = {
+        api1: 9,  // General Knowledge
+        api2: 21, // Sports
+        api3: 23, // History
+        api4: 27, // Animals
+        api5: 15, // Video Games
+        api6: 26, // Celebrities
+    };
 
-let currentQuestionIndex = 0;
-let questionListContainer;
-let data; // Declare data at a broader scope
+    let currentQuestionIndex = 0;
+    let questionListContainer;
+    let data;
+    let gameOver = false;
 
-function searchQuestions() {
-    const categoryDropdown = document.getElementById('categoryDropdown');
-    const selectedCategory = categoryMapping[categoryDropdown.value];
+    function searchQuestions() {
+        const timerDisplay = document.getElementById('timerDisplay')
+        const categoryDropdown = document.getElementById('categoryDropdown');
+        const selectedCategory = categoryMapping[categoryDropdown.value];
 
-    const difficultyDropdown = document.getElementById('difficultyDropdown');
-    const selectedDifficulty = difficultyDropdown.value;
+        const difficultyDropdown = document.getElementById('difficultyDropdown');
+        const selectedDifficulty = difficultyDropdown.value;
 
-    if (!selectedCategory || !selectedDifficulty) {
-        alert('Please select both category and difficulty.');
-        return;
+        if (!selectedCategory || !selectedDifficulty) {
+            alert('Please select both category and difficulty.');
+            return;
+        }
+
+        const apiEndpoint = `https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=${selectedDifficulty}`;
+
+        fetch(apiEndpoint)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(apiData => {
+                data = apiData;
+                console.log(data);
+
+                if (data.results && Array.isArray(data.results)) {
+                    questionListContainer.innerHTML = '';
+                    renderQuestion(currentQuestionIndex, data);
+                } else {
+                    console.error('Invalid data structure:', data);
+                }
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
     }
 
-    const apiEndpoint = `https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=${selectedDifficulty}`;
+    function renderQuestion(index, data) {
+        const source = document.getElementById('question-template').innerHTML;
+        const template = Handlebars.compile(source);
 
-    fetch(apiEndpoint)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
+        const question = data.results[index];
+        question.question = decodeEntities(question.question);
+        question.correct_answer = decodeEntities(question.correct_answer);
+        question.incorrect_answers = question.incorrect_answers.map(decodeEntities);
+
+        const allAnswers = [question.correct_answer, ...question.incorrect_answers];
+
+        shuffleArray(allAnswers);
+
+        question.shuffled_answers = allAnswers;
+
+        const html = template(question);
+        questionListContainer.innerHTML = html;
+
+        startTimer();
+    }
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    let timer;
+    let remainingTime;
+
+    function startTimer() {
+        if (gameOver) {
+            return;
+        }
+    
+        clearInterval(timer);
+    
+        const timeLimitInSeconds = 10;
+    
+        remainingTime = timeLimitInSeconds;
+    
+        updateTimerDisplay();
+    
+        timer = setInterval(() => {
+            remainingTime--;
+            updateTimerDisplay();
+    
+            if (remainingTime <= 0) {
+                scrollQuestions(1);
             }
-            return response.json();
-        })
-        .then(apiData => {
-            data = apiData; // Assign the fetched data to the global variable
-            console.log(data);
-
-            if (data.results && Array.isArray(data.results)) {
-                questionListContainer.innerHTML = '';
-                renderQuestion(currentQuestionIndex, data);
-            } else {
-                console.error('Invalid data structure:', data);
-            }
-        })
-        .catch(error => {
-            console.error('Fetch error:', error);
-        });
-}
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function renderQuestion(index, data) {
-    const source = document.getElementById('question-template').innerHTML;
-    const template = Handlebars.compile(source);
-
-    const question = data.results[index];
-    question.question = decodeEntities(question.question);
-    question.correct_answer = decodeEntities(question.correct_answer);
-    question.incorrect_answers = question.incorrect_answers.map(decodeEntities);
-
-    const allAnswers = [question.correct_answer, ...question.incorrect_answers];
-
-    shuffleArray(allAnswers);
-
-    question.shuffled_answers = allAnswers;
-
-    const html = template(question);
-    questionListContainer.innerHTML = html;
-}
-
-function scrollQuestions(direction) {
-    if (!data) {
-        console.error('Data is not available.');
-        return;
+        }, 1000);
     }
 
-    const totalQuestions = data.results.length;
+    function updateTimerDisplay() {
+        const source = document.getElementById('timer-template').innerHTML;
+        const template = Handlebars.compile(source);
 
-    currentQuestionIndex += direction;
+        const timerData = {
+            timeLeft: remainingTime,
+        };
 
-    if (currentQuestionIndex < 0) {
-        currentQuestionIndex = 0;
-    } else if (currentQuestionIndex >= totalQuestions) {
-        currentQuestionIndex = totalQuestions - 1;
+        const html = template(timerData);
+        document.getElementById('timerDisplay').innerHTML = html;
     }
 
-    renderQuestion(currentQuestionIndex, data);
-}
+    function resetTimer() {
+        clearTimeout(timer);
 
-document.addEventListener('DOMContentLoaded', function () {
-    questionListContainer = document.getElementById('questionList2');
-
-    if (questionListContainer) {
-        questionListContainer.addEventListener('click', function (event) {
-            const button = event.target;
-            if (button.tagName === 'BUTTON') {
-                const answer = button.getAttribute('data-answer');
-                handleAnswerClick(answer);
-            }
-        });
+        startTimer();
     }
-});
 
-function decodeEntities(encodedString) {
-    const parser = new DOMParser();
-    const dom = parser.parseFromString(`<!doctype html><body>${encodedString}`, 'text/html');
-    let decodedString = dom.body.textContent;
+    function scrollQuestions(direction) {
+        if (gameOver || !data) {
+            console.error('Game Over or Data is not available.');
+            return;
+        }
+    
+        const totalQuestions = data.results.length;
+    
+        currentQuestionIndex += direction;
+    
+        if (currentQuestionIndex < 0 || currentQuestionIndex >= totalQuestions) {
+            stopTimer(); // Stop the timer when the game is over
+            displayGameOverMessage(); // Display Game Over message
+            return;
+        }
+    
+        renderQuestion(currentQuestionIndex, data);
+        startTimer();
+    }
+    
 
-    decodedString = decodedString.replace(/&quot;/g, '"');
-    decodedString = decodedString.replace(/&amp;/g, '&');
-    decodedString = decodedString.replace(/&lt;/g, '<');
-    decodedString = decodedString.replace(/&gt;/g, '>');
-    decodedString = decodedString.replace(/&#039;/g, "'");
-    decodedString = decodedString.replace(/&lsquo;/g, "'");
-    decodedString = decodedString.replace(/&rsquo;/g, "'");
-    decodedString = decodedString.replace(/&ldquo;/g, '"');
-    decodedString = decodedString.replace(/&rdquo;/g, '"');
+    function displayGameOverMessage() {
+        if (!gameOver) {
+            gameOver = true;
+            alert('Game Over');
+        }
+    }
+    
+    function stopTimer() {
+        clearInterval(timer);
+    }
 
-    return decodedString;
-}
+    document.addEventListener('DOMContentLoaded', function () {
+        questionListContainer = document.getElementById('questionList2');
+
+        if (questionListContainer) {
+            questionListContainer.addEventListener('click', function (event) {
+                const button = event.target;
+                if (button.tagName === 'BUTTON') {
+                    const answer = button.getAttribute('data-answer');
+                    // handleAnswerClick(answer);
+                    resetTimer();
+                }
+            });
+        }
+
+    
+
+    });
+
+    function decodeEntities(encodedString) {
+        const parser = new DOMParser();
+        const dom = parser.parseFromString(`<!doctype html><body>${encodedString}`, 'text/html');
+        let decodedString = dom.body.textContent;
+
+        decodedString = decodedString.replace(/&quot;/g, '"');
+        decodedString = decodedString.replace(/&amp;/g, '&');
+        decodedString = decodedString.replace(/&lt;/g, '<');
+        decodedString = decodedString.replace(/&gt;/g, '>');
+        decodedString = decodedString.replace(/&#039;/g, "'");
+        decodedString = decodedString.replace(/&lsquo;/g, "'");
+        decodedString = decodedString.replace(/&rsquo;/g, "'");
+        decodedString = decodedString.replace(/&ldquo;/g, '"');
+        decodedString = decodedString.replace(/&rdquo;/g, '"');
+
+        return decodedString;
+    }
+
+    // Other functions or setup can go here
+
